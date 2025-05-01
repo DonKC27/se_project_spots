49a7fd070ee7096dbc632f111b43defde5f64719
Don KC <edwinjrmiller15@gmail.com>
2025-05-01 00:32:17 -0500

    Sprint 6 final corrections

--- scripts/index.js
+++ scripts/index.js
@@ -61,9 +61,12 @@ function handleEscapeKey(evt) {
 
 function openModal(modal) {
   modal.classList.add("modal_is-opened");
+  document.addEventListener("keydown", handleEscapeKey);
 }
+
 function closeModal(modal) {
   modal.classList.remove("modal_is-opened");
+  document.removeEventListener("keydown", handleEscapeKey);
 }
 
 function handleEditProfileSubmit(evt) {
